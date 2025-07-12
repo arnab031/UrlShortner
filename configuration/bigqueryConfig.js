7fe31890c2f8eebb322d37c0cd67d304bdd906d5
arnab031 <51916075+arnab031@users.noreply.github.com>
2025-07-12 19:38:24 +0530

    Changes

--- configuration/bigqueryConfig.js
+++ configuration/bigqueryConfig.js
@@ -1,8 +1,9 @@
 const { BigQuery } = require("@google-cloud/bigquery");
 
-const bigquery = new BigQuery({
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  keyFilename: "./configuration/firebaseConfig.json",
-});
+// const bigquery = new BigQuery({
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   keyFilename: "./configuration/firebaseConfig.json",
+// });
+const bigquery = new BigQuery(); // Uses application default credentials
 
 module.exports = bigquery;
